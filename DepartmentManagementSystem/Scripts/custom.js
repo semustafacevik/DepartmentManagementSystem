e14044c53247225de89d8c2066e3b32e91167abd
Mustafa_CEVIK <cevikksoft@gmail.com>
2019-08-17 03:27:30 +0300

    Use ajax for deletion & add DataTable.

--- DepartmentManagementSystem/Scripts/custom.js
+++ DepartmentManagementSystem/Scripts/custom.js
@@ -1,4 +1,27 @@
-﻿function CheckDateTypeIsValid(dateElement) {
+﻿$(function () {
+    $("#tblDepartments").DataTable()
+    $("#tblPersonnels").DataTable()
+    $("#tblDepartments").on("click", ".btnDeleteDepartment", function () {
+        var id = $(this).data("id")
+        var name = $(this).data("name") + " Department"
+        var btn = $(this)
+        bootbox.confirm(name.bold().italics() + " will be deleted! Are you sure?", function (result) {
+            if (result) {
+                $.ajax({
+                    type: "GET",
+                    url: "Department/Delete/" + id,
+                    success: function () {
+                        btn.parent().parent().remove()
+                    }
+
+                })
+            }
+        })
+    })
+
+})
+
+function CheckDateTypeIsValid(dateElement) {
     var value = $(dateElement).val();
     if (value == '') {
         $(dateElement).valid("false");
