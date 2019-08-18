71540bd6d8d394841e722596d4f529714a4e6d08
Mustafa_CEVIK <cevikksoft@gmail.com>
2019-08-18 20:10:51 +0300

    Done authentication & add login page & edit navbar.

---
+++ DepartmentManagementSystem/Controllers/SecurityController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DepartmentManagementSystem.Models.EntityFramework;
+using System.Web.Security;
+
+namespace DepartmentManagementSystem.Controllers
+{
+    public class SecurityController : Controller
+    {
+        DepartmentManagementDBEntities db = new DepartmentManagementDBEntities();
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(tblUser user)
+        {
+            var userInDB = db.tblUser.FirstOrDefault(m => m.u_Name == user.u_Name && m.u_Password == user.u_Password);
+
+            if (userInDB != null)
+            {
+                FormsAuthentication.SetAuthCookie(userInDB.u_Name, false);
+                return RedirectToAction("Index", "Department");
+            }
+
+            else
+            {
+                ViewBag.Message = "XXXXX";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
